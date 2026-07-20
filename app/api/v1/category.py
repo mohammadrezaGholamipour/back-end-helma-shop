fbc1be65ad4918714151e453fb4e4184aba72449
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-07-20 21:30:49 +0000

    feat(extention and display-order):add extention fo python formatt and completed order for categerory

--- app/api/v1/category.py
+++ app/api/v1/category.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Form, UploadFile, File, HTTPException ,Query
+from fastapi import APIRouter, Depends, Form, UploadFile, File, HTTPException, Query
 from app.schemas.category import CategoryOut, CreateAndUpdateCategory
 from app.core.security import get_current_user
 from app.models.category import Category
@@ -14,30 +14,42 @@ router = APIRouter(prefix="/helma-shop-api/v1/category", tags=["Category"])
 UPLOAD_DIR = "uploads/categories"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+
 # ===================== create =====================
 @router.post("/create", response_model=CategoryOut)
 def create_category(
     name: str = Form(...),
     slug: str = Form(...),  # اضافه شد برای سئو
-    meta_title: str | None = Form(None), # اضافه شد
-    meta_description: str | None = Form(None), # اضافه شد
+    meta_title: str | None = Form(None),  # اضافه شد
+    meta_description: str | None = Form(None),  # اضافه شد
     image: UploadFile = File(None),
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ):
     # بررسی تکراری نبودن نام
     if db.query(Category).filter(Category.name == name).first():
-        raise HTTPException(status_code=400, detail={"field": "name", "message": "این نام قبلاً ثبت شده است"})
-    
+        raise HTTPException(
+            status_code=400,
+            detail={"field": "name", "message": "این نام قبلاً ثبت شده است"},
+        )
+
     # بررسی تکراری نبودن اسلاگ (حیاتی برای سئو)
     if db.query(Category).filter(Category.slug == slug).first():
-        raise HTTPException(status_code=400, detail={"field": "slug", "message": "این اسلاگ (URL) قبلاً استفاده شده است"})
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "field": "slug",
+                "message": "این اسلاگ (URL) قبلاً استفاده شده است",
+            },
+        )
 
     image_url = None
     if image:
         if not image.content_type.startswith("image/"):
-            raise HTTPException(status_code=400, detail={"message": "فایل باید تصویر باشد"})
-        
+            raise HTTPException(
+                status_code=400, detail={"message": "فایل باید تصویر باشد"}
+            )
+
         ext = os.path.splitext(image.filename)[1]
         filename = f"{uuid.uuid4().hex}{ext}"
         file_path = os.path.join(UPLOAD_DIR, filename)
@@ -45,14 +57,13 @@ def create_category(
         with open(file_path, "wb") as buffer:
             buffer.write(image.file.read())
         image_url = f"/uploads/categories/{filename}"
-        
-        
+
     last_order = db.query(func.max(Category.display_order)).scalar() or 0
 
     category = Category(
         application_id=current_user.application_id,
         meta_description=meta_description,
-         display_order=last_order + 1,
+        display_order=last_order + 1,
         owner_id=current_user.id,
         meta_title=meta_title,
         image=image_url,
@@ -65,26 +76,35 @@ def create_category(
     db.refresh(category)
     return category
 
+
 # ===================== list me =====================
 @router.get("/me", response_model=list[CategoryOut])
 def get_categories(
-    application_id: int | None = None, # معمولا آیدی ها int هستند
+    application_id: int | None = None,  # معمولا آیدی ها int هستند
     db: Session = Depends(get_db),
 ):
     if not application_id:
-        raise HTTPException(status_code=400, detail={"message": "شناسه اپلیکیشن الزامی است"})
-        
-    return db.query(Category).filter(Category.application_id == application_id).all()
+        raise HTTPException(
+            status_code=400, detail={"message": "شناسه اپلیکیشن الزامی است"}
+        )
+
+    return (
+        db.query(Category)
+        .filter(Category.application_id == application_id)
+        .order_by(Category.display_order.asc())
+        .all()
+    )
+
 
 # ===================== delete =====================
 @router.delete("/delete/{category_id}")
 def delete_category(
     category_id: int,
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ):
     category = db.query(Category).filter(Category.id == category_id).first()
-    
+
     if not category:
         raise HTTPException(status_code=404, detail={"message": "یافت نشد"})
 
@@ -94,6 +114,7 @@ def delete_category(
     db.delete(category)
     db.commit()
     return {"message": "دسته بندی و تمام محصولات زیرمجموعه حذف شدند"}
+
 
 # ===================== get category products =====================
 @router.get("/{slug}")
@@ -108,7 +129,9 @@ def get_category_products(
     category_query = db.query(Category).filter(Category.slug == slug)
 
     if application_id:
-        category_query = category_query.filter(Category.application_id == application_id)
+        category_query = category_query.filter(
+            Category.application_id == application_id
+        )
 
     category = category_query.first()
 
@@ -122,12 +145,7 @@ def get_category_products(
 
     total = products_query.count()
 
-    products = (
-        products_query
-        .offset((page - 1) * per_page)
-        .limit(per_page)
-        .all()
-    )
+    products = products_query.offset((page - 1) * per_page).limit(per_page).all()
 
     last_page = (total + per_page - 1) // per_page
 
@@ -140,6 +158,7 @@ def get_category_products(
         "last_page": last_page,
     }
 
+
 # ===================== update =====================
 @router.put("/update", response_model=CategoryOut)
 def update_category(
@@ -151,7 +170,7 @@ def update_category(
     meta_description: str | None = Form(None),
     image: UploadFile = File(None),
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ):
     category = db.query(Category).filter(Category.id == category_id).first()
     if not category:
@@ -162,16 +181,29 @@ def update_category(
 
     if name:
         category.name = name
-    
+        
+        
+    if display_order:
+        category.display_order = display_order
+
     if slug:
         # چک کردن اینکه اسلاگ جدید با اسلاگ دیگران تداخل نداشته باشد
-        existing = db.query(Category).filter(Category.slug == slug, Category.id != category_id).first()
+        existing = (
+            db.query(Category)
+            .filter(Category.slug == slug, Category.id != category_id)
+            .first()
+        )
         if existing:
-            raise HTTPException(status_code=400, detail={"message": "این اسلاگ قبلاً توسط دسته دیگری رزرو شده است"})
+            raise HTTPException(
+                status_code=400,
+                detail={"message": "این اسلاگ قبلاً توسط دسته دیگری رزرو شده است"},
+            )
         category.slug = slug
 
-    if meta_title: category.meta_title = meta_title
-    if meta_description: category.meta_description = meta_description
+    if meta_title:
+        category.meta_title = meta_title
+    if meta_description:
+        category.meta_description = meta_description
 
     if image:
         ext = os.path.splitext(image.filename)[1]
